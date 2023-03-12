a52dcfbb4bb3146bd1a50fedaa47b0f4433ca04c
PodoprigoraIvan <shtarmel@gmail.com>
2023-03-12 20:23:35 +0300

    Changed to .NET Framework 4.8

--- Setup_database_for_device/Setup_database_for_device/Form1.cs
+++ Setup_database_for_device/Setup_database_for_device/Form1.cs
@@ -17,5 +17,9 @@ namespace Setup_database_for_device
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Кнопка нажата";
+        }
     }
 }
