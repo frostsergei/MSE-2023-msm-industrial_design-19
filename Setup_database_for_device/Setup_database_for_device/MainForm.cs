ac89adafbdf460071f57c3f8ca7412ff8b1a7147
Ivan <57725067+PodoprigoraIvan@users.noreply.github.com>
2023-04-14 15:24:38 +0300

    #32: Разработка класса-окна для общего вида приложения

---
+++ Setup_database_for_device/Setup_database_for_device/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Setup_database_for_device
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            //DB.Test test = new DB.Test();
+            TestForm subForm = new TestForm();
+            subForm.TopLevel = false;
+            subForm.AutoScroll = true;
+            subForm.Dock = DockStyle.Fill;
+            subForm.FormBorderStyle = FormBorderStyle.None;
+            panelContent.Controls.Add(subForm);
+            subForm.BringToFront();
+            subForm.Show();
+        }
+    }
+}
