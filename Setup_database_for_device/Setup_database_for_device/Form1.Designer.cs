17d19075ef402d35af75f9a52dddbbfe12278088
Vladis4791 <vladislav.parfenov05@gmail.com>
2023-04-17 13:04:35 +0300

    Created a content Menu Control
    There was a major part of creating Menu Control

--- Setup_database_for_device/Setup_database_for_device/Form1.Designer.cs
+++ Setup_database_for_device/Setup_database_for_device/Form1.Designer.cs
@@ -30,16 +30,18 @@ namespace Setup_database_for_device
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
             this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.contentMenu1 = new Setup_database_for_device.View.ContentMenu();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,65 +61,15 @@ namespace Setup_database_for_device
             this.panel1.Size = new System.Drawing.Size(421, 85);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // checkBox6
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(3, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Выбор каналов:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(3, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(160, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Выбор потребителей:";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(130, 10);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(45, 21);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "к1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(181, 10);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(45, 21);
-            this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "к2";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
-            // checkBox3
-            // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(232, 10);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(45, 21);
-            this.checkBox3.TabIndex = 4;
-            this.checkBox3.Text = "к3";
-            this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox4
-            // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(169, 47);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(46, 21);
-            this.checkBox4.TabIndex = 5;
-            this.checkBox4.Text = "п1";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(273, 47);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(46, 21);
+            this.checkBox6.TabIndex = 7;
+            this.checkBox6.Text = "п3";
+            this.checkBox6.UseVisualStyleBackColor = true;
             // 
             // checkBox5
             // 
@@ -129,19 +81,69 @@ namespace Setup_database_for_device
             this.checkBox5.Text = "п2";
             this.checkBox5.UseVisualStyleBackColor = true;
             // 
-            // checkBox6
+            // checkBox4
             // 
-            this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(273, 47);
-            this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(46, 21);
-            this.checkBox6.TabIndex = 7;
-            this.checkBox6.Text = "п3";
-            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(169, 47);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(46, 21);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "п1";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(232, 10);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(45, 21);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "к3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(181, 10);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(45, 21);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "к2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(130, 10);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(45, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "к1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выбор потребителей:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выбор каналов:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 117);
+            this.button1.Location = new System.Drawing.Point(12, 392);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(132, 46);
             this.button1.TabIndex = 1;
@@ -158,12 +160,22 @@ namespace Setup_database_for_device
             this.label3.Size = new System.Drawing.Size(0, 25);
             this.label3.TabIndex = 2;
             // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(637, 45);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(291, 434);
+            this.elementHost1.TabIndex = 3;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.contentMenu1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1016, 628);
+            this.Controls.Add(this.elementHost1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
@@ -189,6 +201,8 @@ namespace Setup_database_for_device
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private View.ContentMenu contentMenu1;
     }
 }
 
---
+++ Setup_database_for_device/Setup_database_for_device/View/ContentMenu.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Setup_database_for_device.View
+{
+
+    public partial class ContentMenu : UserControl
+    {
+        private static readonly string s_topTreeGroupName = "ContentButtons";
+        private static readonly string s_pipelinesTreeGroupName = "PipelinesButtons";
+        private static readonly string s_consumersTreeGroupName = "ConsumersButtons";
+        private static readonly string s_pipelinePrefix = "Трубопровод";
+        private static readonly string s_consumersPrefix = "Потребитель";
+
+        private TreeViewItem[] _pipelinesTreeViewItems;
+        private TreeViewItem[] _consumersTreeViewItems;
+
+        private int _pipelineCount = 2;
+        private int _consumersCount = 2;
+
+        public ContentMenu()
+        {
+
+
+            InitializeComponent();
+            _pipelinesTreeViewItems = new TreeViewItem[_pipelineCount];
+
+            _pipelinesTreeViewItems = GetTreeViewItems(GetNamesArray(s_pipelinePrefix, _pipelineCount), s_pipelinesTreeGroupName);
+            _consumersTreeViewItems = GetTreeViewItems(GetNamesArray(s_consumersPrefix, _consumersCount), s_consumersTreeGroupName);
+
+            TreeViewItem pipelinesSettings = FindName("PipelinesSettings") as TreeViewItem;
+            TreeViewItem consumersSettings = FindName("ConsumersSettings") as TreeViewItem;
+
+            foreach (TreeViewItem item in _pipelinesTreeViewItems)
+            {
+                pipelinesSettings.Items.Add(item);
+            }
+
+            foreach (TreeViewItem item in _consumersTreeViewItems)
+            {
+                consumersSettings.Items.Add(item);
+            }
+
+        }
+
+        private RadioButton GetTreeViewItemInnerElement(string name, string groupName)
+        {
+
+            Style style = FindResource("SimpleButton") as Style;
+
+            return new RadioButton
+            {
+                Content = name,
+                GroupName = groupName,
+                IsEnabled = true,
+                Style = style,
+                Width = 130,
+                Margin = new Thickness(0, 10, 0, 0)
+        };
+
+        }
+
+        private string[] GetNamesArray(string prefix, int elementCount)
+        {
+            string[] names = new string[elementCount];
+
+            for(int i = 0; i < elementCount; i++)
+            {
+                names[i] = prefix + (i + 1).ToString();
+            }
+
+            return names;
+        }
+
+        private TreeViewItem[] GetTreeViewItems(string[] names, string groupName)
+        {
+
+            TreeViewItem[] result = new TreeViewItem[names.Length];
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                TreeViewItem currentTopTreeViewItem = new TreeViewItem();
+
+                RadioButton radioButtonInTreeViewItem = GetTreeViewItemInnerElement(names[i], groupName);
+                radioButtonInTreeViewItem.Name = s_topTreeGroupName + (i + 1).ToString();
+
+                currentTopTreeViewItem.Header = radioButtonInTreeViewItem;
+
+                result[i] = currentTopTreeViewItem;
+
+            }
+
+            return result;
+
+        }
+    }
+}
