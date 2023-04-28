207189374ee736a629c9f439ed70b33f4b89dd0f
Ivan <57725067+PodoprigoraIvan@users.noreply.github.com>
2023-04-28 14:47:15 +0300

    #58: Изменение вариантов номера схемы учета на окне ввода параметров потребителей

--- Setup_database_for_device/Setup_database_for_device/View/ConsumerForm.Designer.cs
+++ Setup_database_for_device/Setup_database_for_device/View/ConsumerForm.Designer.cs
@@ -29,80 +29,85 @@ namespace Setup_database_for_device.View
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConsumerForm));
             this.labelConsumerTitle = new System.Windows.Forms.Label();
             this.labelConsumerId = new System.Windows.Forms.Label();
             this.numericUpDownConsumerId = new System.Windows.Forms.NumericUpDown();
             this.labelCountingScheme = new System.Windows.Forms.Label();
-            this.comboBoxCountingScheme = new System.Windows.Forms.ComboBox();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.schemeNumberControl = new Setup_database_for_device.View.SchemeNumberControl();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownConsumerId)).BeginInit();
             this.SuspendLayout();
             // 
             // labelConsumerTitle
             // 
             this.labelConsumerTitle.AutoSize = true;
-            this.labelConsumerTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelConsumerTitle.Location = new System.Drawing.Point(12, 20);
+            this.labelConsumerTitle.Font = new System.Drawing.Font("Verdana", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelConsumerTitle.Location = new System.Drawing.Point(14, 22);
             this.labelConsumerTitle.Name = "labelConsumerTitle";
-            this.labelConsumerTitle.Size = new System.Drawing.Size(272, 39);
+            this.labelConsumerTitle.Size = new System.Drawing.Size(193, 26);
             this.labelConsumerTitle.TabIndex = 0;
             this.labelConsumerTitle.Text = "Потребитель №";
             // 
             // labelConsumerId
             // 
             this.labelConsumerId.AutoSize = true;
-            this.labelConsumerId.Location = new System.Drawing.Point(16, 80);
+            this.labelConsumerId.Location = new System.Drawing.Point(18, 62);
             this.labelConsumerId.Name = "labelConsumerId";
-            this.labelConsumerId.Size = new System.Drawing.Size(204, 17);
+            this.labelConsumerId.Size = new System.Drawing.Size(229, 18);
             this.labelConsumerId.TabIndex = 1;
             this.labelConsumerId.Text = "Идентификатор потребителя";
             // 
             // numericUpDownConsumerId
             // 
-            this.numericUpDownConsumerId.Location = new System.Drawing.Point(246, 78);
+            this.numericUpDownConsumerId.Location = new System.Drawing.Point(277, 60);
             this.numericUpDownConsumerId.Maximum = new decimal(new int[] {
             999999,
             0,
             0,
             0});
             this.numericUpDownConsumerId.Name = "numericUpDownConsumerId";
-            this.numericUpDownConsumerId.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownConsumerId.Size = new System.Drawing.Size(135, 26);
             this.numericUpDownConsumerId.TabIndex = 2;
             // 
             // labelCountingScheme
             // 
             this.labelCountingScheme.AutoSize = true;
-            this.labelCountingScheme.Location = new System.Drawing.Point(83, 115);
+            this.labelCountingScheme.Location = new System.Drawing.Point(18, 99);
             this.labelCountingScheme.Name = "labelCountingScheme";
-            this.labelCountingScheme.Size = new System.Drawing.Size(137, 17);
+            this.labelCountingScheme.Size = new System.Drawing.Size(158, 18);
             this.labelCountingScheme.TabIndex = 3;
             this.labelCountingScheme.Text = "Номер схемы учета";
             // 
-            // comboBoxCountingScheme
+            // imageList
             // 
-            this.comboBoxCountingScheme.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxCountingScheme.FormattingEnabled = true;
-            this.comboBoxCountingScheme.Items.AddRange(new object[] {
-            "0",
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"});
-            this.comboBoxCountingScheme.Location = new System.Drawing.Point(246, 115);
-            this.comboBoxCountingScheme.Name = "comboBoxCountingScheme";
-            this.comboBoxCountingScheme.Size = new System.Drawing.Size(120, 24);
-            this.comboBoxCountingScheme.TabIndex = 4;
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "scheme_0.jpg");
+            this.imageList.Images.SetKeyName(1, "scheme_1.jpg");
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(21, 131);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(582, 127);
+            this.elementHost1.TabIndex = 5;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.schemeNumberControl;
             // 
             // ConsumerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.comboBoxCountingScheme);
+            this.ClientSize = new System.Drawing.Size(900, 632);
+            this.Controls.Add(this.elementHost1);
             this.Controls.Add(this.labelCountingScheme);
             this.Controls.Add(this.numericUpDownConsumerId);
             this.Controls.Add(this.labelConsumerId);
             this.Controls.Add(this.labelConsumerTitle);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Name = "ConsumerForm";
             this.Text = "ConsumerForm";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownConsumerId)).EndInit();
@@ -117,6 +122,8 @@ namespace Setup_database_for_device.View
         private System.Windows.Forms.Label labelConsumerId;
         private System.Windows.Forms.NumericUpDown numericUpDownConsumerId;
         private System.Windows.Forms.Label labelCountingScheme;
-        private System.Windows.Forms.ComboBox comboBoxCountingScheme;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private SchemeNumberControl schemeNumberControl;
     }
 }
