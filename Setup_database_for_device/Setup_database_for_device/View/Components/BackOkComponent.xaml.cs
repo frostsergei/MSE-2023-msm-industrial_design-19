9a91b73777c93028f71e51565f214299341ad552
Vladis4791 <vladislav.parfenov05@gmail.com>
2023-05-07 10:25:32 +0300

    Created a frame of FormSwitcher
     add event listeners
    add a new class WindowForm
    created a component of okBack buttons

---
+++ Setup_database_for_device/Setup_database_for_device/View/Components/BackOkComponent.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Setup_database_for_device.View.Components
+{
+    public partial class BackOkComponent : UserControl
+    {
+
+        public event EventHandler LeftButtonClickedEvent;
+        public event EventHandler RightButtonClickedEvent;
+
+        public BackOkComponent()
+        {
+            InitializeComponent();
+        }
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            LeftButtonClickedEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
+            RightButtonClickedEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
