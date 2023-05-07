1035fc9a1a86ce37d5affb35349f84d941748c3f
Vladis4791 <vladislav.parfenov05@gmail.com>
2023-05-07 10:25:32 +0300

    Created a frame of FormSwitcher
     add event listeners
    add a new class WindowForm
    created a component of okBack buttons

---
+++ Setup_database_for_device/Setup_database_for_device/FormSwitcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Setup_database_for_device
+{
+    class FormSwitcher
+    {
+
+        private View.ContentMenu _menu;
+        private View.WindowForm[] _forms;
+        private int currentFormIndex = 0;
+
+        public FormSwitcher(View.ContentMenu menu, View.WindowForm[] forms, Panel contentPanel)
+        {
+            _menu = menu;
+            _forms = forms;
+
+            _menu.FormChanged += new EventHandler(ChangeFormByClickOnMenu);
+
+            foreach(View.WindowForm form in _forms)
+            {
+                form.NextFormEvent += new EventHandler(GoAhead);
+                form.PreviousFormEvent += new EventHandler(GoBack);
+            }
+        }
+
+        //private Form GetFormBy
+
+        public void ChangeFormByClickOnMenu(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GoBack(object sender, EventArgs e) 
+        {
+
+        }
+
+        public void GoAhead(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Setup_database_for_device/Setup_database_for_device/View/WindowForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Setup_database_for_device.View
+{
+    public abstract partial class WindowForm : Form
+    {
+
+        public event EventHandler NextFormEvent;
+        public event EventHandler PreviousFormEvent;
+
+        public WindowForm()
+        {
+            Components.BackOkComponent backOkComponent = new Components.BackOkComponent();
+            backOkComponent.BackButtonClickedEvent += new EventHandler(GoToPreviousForm);
+            backOkComponent.OkButtonClickedEvent += new EventHandler(GoToNextForm);
+        }
+
+        private string _formName;
+
+        public string FormName => _formName;
+
+        public void GoToNextForm(object sender, EventArgs e) {
+            NextFormEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void GoToPreviousForm(object sender, EventArgs e) {
+            PreviousFormEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
