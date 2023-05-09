52beda790801c5e2c8da6fd8cde046b7f874fdba
AlexandrovichValeria <71718459+AlexandrovichValeria@users.noreply.github.com>
2023-05-09 22:21:35 +0300

    #75: Cохранение параметров с окна выбора теплоносителя в модель

---
+++ Setup_database_for_device/Setup_database_for_device/Controller/CoolantController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Setup_database_for_device.Controller
+{
+    public class CoolantController : Controller
+    {
+        private View.CoolantSelectionForm _view;
+
+        private Model.Model _model;
+
+        private Model.Pipeline _pipelineModel;
+
+        public CoolantController(View.CoolantSelectionForm view, Model.Model model, int index)
+        {
+            _view = view;
+            _model = model;
+            _pipelineModel = model.GetPipelineByInd(index);
+        }
+        public override void SaveDataToModel()
+        {
+            Dictionary<string, string> currentData = _view.GetCoolantWindowData();
+
+            foreach (KeyValuePair<string, string> paramValuePair in currentData)
+            {
+                if (paramValuePair.Value != "")
+                {
+                    _pipelineModel.ChangeParameterValue(paramValuePair.Key, paramValuePair.Value);
+                }
+            }
+        }
+    }
+}
