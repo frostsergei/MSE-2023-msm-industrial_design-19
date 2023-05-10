409ffbfe9c5ddacc60f8496c39029eb4538efb7f
vbezuprechnaya <vberezenok@gmail.com>
2023-05-10 20:48:11 +0300

    Pipeline window vers 1, without model&adaptation

--- Setup_database_for_device/Setup_database_for_device/MainForm.Designer.cs
+++ Setup_database_for_device/Setup_database_for_device/MainForm.Designer.cs
@@ -42,12 +42,14 @@ namespace Setup_database_for_device
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.файлToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(986, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1479, 42);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -56,13 +58,13 @@ namespace Setup_database_for_device
             this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.сохранитьToolStripMenuItem});
             this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(90, 42);
             this.файлToolStripMenuItem.Text = "Файл";
             // 
             // сохранитьToolStripMenuItem
             // 
             this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(263, 44);
             this.сохранитьToolStripMenuItem.Text = "Сохранить";
             // 
             // panelLeft
@@ -70,18 +72,20 @@ namespace Setup_database_for_device
             this.panelLeft.BackColor = System.Drawing.Color.Gainsboro;
             this.panelLeft.Controls.Add(this.label1);
             this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLeft.Location = new System.Drawing.Point(0, 28);
+            this.panelLeft.Location = new System.Drawing.Point(0, 42);
+            this.panelLeft.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelLeft.Name = "panelLeft";
-            this.panelLeft.Size = new System.Drawing.Size(168, 541);
+            this.panelLeft.Size = new System.Drawing.Size(252, 847);
             this.panelLeft.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Location = new System.Drawing.Point(18, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 29);
+            this.label1.Size = new System.Drawing.Size(214, 44);
             this.label1.TabIndex = 0;
             this.label1.Text = "Навигация";
             // 
@@ -89,21 +93,23 @@ namespace Setup_database_for_device
             // 
             this.panelContent.BackColor = System.Drawing.SystemColors.Info;
             this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContent.Location = new System.Drawing.Point(168, 28);
+            this.panelContent.Location = new System.Drawing.Point(252, 42);
+            this.panelContent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(818, 541);
+            this.panelContent.Size = new System.Drawing.Size(1227, 847);
             this.panelContent.TabIndex = 5;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Menu;
-            this.ClientSize = new System.Drawing.Size(986, 569);
+            this.ClientSize = new System.Drawing.Size(1479, 889);
             this.Controls.Add(this.panelContent);
             this.Controls.Add(this.panelLeft);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.Text = "Настройщик базы данных";
             this.menuStrip1.ResumeLayout(false);
---
+++ Setup_database_for_device/Setup_database_for_device/View/PipelineSettingLimits.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Windows.Documents;
+
+namespace Setup_database_for_device.View
+{
+    public partial class PipelineSettingLimits : Form
+    { 
+        string curIndicator = "04";// убрать после подсодинения к моделе
+        public PipelineSettingLimits()
+        {
+            InitializeComponent();
+                       
+            this.Text = "Настройка трубопроводов. Ввод значения расхода, избыточного давления и температуры теплоносителя";
+        }
+        
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PipelineSettingLimits_Load(object sender, EventArgs e)
+        {
+           if(curIndicator == "03" || curIndicator == "04")
+            {
+                richTextBox1.Visible = true;
+                richTextBox2.Visible = true;
+
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+                textBox4.Visible = true;
+
+                label1.Visible = true;
+                label2.Visible = true;
+
+                label3.Visible = true;
+                label4.Visible = true;
+
+                //turning off
+
+                richTextBox13.Visible = false;
+                textBox13.Visible = false;
+                label9.Visible = false;
+                label10.Visible = false;
+                textBox14.Visible = false;
+
+                textBox15.Visible = false;
+                richTextBox14.Visible = false;
+            }
+            if (curIndicator == "01" || curIndicator == "02")
+            {
+                richTextBox13.Visible = true;
+                textBox13.Visible = true;
+                label9.Visible = true;
+                label10.Visible = true;
+                textBox14.Visible = true;
+
+                textBox15.Visible = true;
+                richTextBox14.Visible = true;
+
+            //turning off
+                richTextBox1.Visible = false;
+                richTextBox2.Visible = false;
+
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                textBox4.Visible = false;
+
+                label1.Visible = false;
+                label2.Visible = false;
+
+                label3.Visible = false;
+                label4.Visible = false;
+            }
+        }        
+    }
+
+}
