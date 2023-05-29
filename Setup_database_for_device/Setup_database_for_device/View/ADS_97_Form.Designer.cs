8787cc0e6d4d5f75eb27bad88ddc62a31e1d79d5
PodoprigoraIvan <shtarmel@gmail.com>
2023-05-29 21:43:33 +0300

    added ADS97 window and controller

---
+++ Setup_database_for_device/Setup_database_for_device/View/ADS_97_Form.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Setup_database_for_device.View
+{
+    partial class ADS_97_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelFirstAdapter = new System.Windows.Forms.Label();
+            this.labelSecondAdapter = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.buttonOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(28, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Необходимо применить АДС-97,\r\nиначе продолжение невозможно";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(30, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Количество адаптеров";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.comboBox1.Location = new System.Drawing.Point(246, 113);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(109, 26);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // labelFirstAdapter
+            // 
+            this.labelFirstAdapter.AutoSize = true;
+            this.labelFirstAdapter.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFirstAdapter.Location = new System.Drawing.Point(70, 168);
+            this.labelFirstAdapter.Name = "labelFirstAdapter";
+            this.labelFirstAdapter.Size = new System.Drawing.Size(143, 18);
+            this.labelFirstAdapter.TabIndex = 3;
+            this.labelFirstAdapter.Text = "Адрес адаптера 1";
+            // 
+            // labelSecondAdapter
+            // 
+            this.labelSecondAdapter.AutoSize = true;
+            this.labelSecondAdapter.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSecondAdapter.Location = new System.Drawing.Point(70, 219);
+            this.labelSecondAdapter.Name = "labelSecondAdapter";
+            this.labelSecondAdapter.Size = new System.Drawing.Size(143, 18);
+            this.labelSecondAdapter.TabIndex = 4;
+            this.labelSecondAdapter.Text = "Адрес адаптера 2";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(246, 164);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            29,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(109, 22);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(246, 219);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            29,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(109, 22);
+            this.numericUpDown2.TabIndex = 6;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonOK.Location = new System.Drawing.Point(229, 259);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(94, 41);
+            this.buttonOK.TabIndex = 7;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // ADS_97_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 326);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.labelSecondAdapter);
+            this.Controls.Add(this.labelFirstAdapter);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "ADS_97_Form";
+            this.Text = "Применение АДС-97";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelFirstAdapter;
+        private System.Windows.Forms.Label labelSecondAdapter;
+        private System.Windows.Forms.Button buttonOK;
+        public System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.NumericUpDown numericUpDown1;
+        public System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
---
+++ Setup_database_for_device/Setup_database_for_device/View/ADS_97_Form.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Setup_database_for_device.View
+{
+    public partial class ADS_97_Form : Form
+    {
+        public ADS_97_Form()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            labelSecondAdapter.Visible = false;
+            numericUpDown2.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                labelSecondAdapter.Visible = false;
+                numericUpDown2.Visible = false;
+            }
+            else
+            {
+                labelSecondAdapter.Visible = true;
+                numericUpDown2.Visible = true;
+            }
+        }
+    }
+}
